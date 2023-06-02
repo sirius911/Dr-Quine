010e4d1d2d36557c61e795a44a1d26a01eb816e1
clorin <clorin@student.42.fr>
2023-06-02 12:31:19 +0200

    Colleen in C OK

---
+++ C/Colleen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//comment outside
+
+void write(char *str)
+{
+    printf(str, 10, 34, str);
+}
+
+int main(void)
+{
+    //comment in main
+    char *x = "#include <stdio.h>%1$c//comment outside%1$c%1$cvoid write(char *str)%1$c{%1$c    printf(str, 10, 34, str);%1$c}%1$c%1$cint main(void)%1$c{%1$c    //comment in main%1$c    char *x = %2$c%3$s%2$c;%1$c    write(x);%1$c}";
+    write(x);
+}
--- python/Colleen.py
+++ python/Colleen.py
@@ -1,11 +1,11 @@
-#comment ouside
+#comment outside
 
 def write(x):
     print(x % x)
 
 def main():
   #comment in main
-  x = '#comment ouside\n\ndef write(x):\n    print(x %% x)\n\ndef main():\n  #comment in main\n  x = %r\n  write(x)\n\nmain()'
+  x = '#comment outside\n\ndef write(x):\n    print(x %% x)\n\ndef main():\n  #comment in main\n  x = %r\n  write(x)\n\nmain()'
   write(x)
 
 main()
