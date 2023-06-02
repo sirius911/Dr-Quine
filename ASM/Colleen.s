c987d423b5ba3d4574101328d5cc84cb23fe038f
clorin <clorin@student.42.fr>
2023-06-02 17:24:26 +0200

    Div Fix plus ASM Colleen OK

---
+++ ASM/Colleen.s
@@ -0,0 +1,22 @@
+;colleen
+
+section .text
+global main
+extern printf
+
+main:
+push rbp
+mov rbp, rsp
+mov rdi, format
+mov rsi, format
+mov rdx, 10
+mov rcx, 34
+call printf
+mov rsp, rbp
+pop rbp
+xor rax, rax
+ret
+
+section .data
+format:
+db ";colleen%2$c%2$csection .text%2$cglobal main%2$cextern printf%2$c%2$cmain:%2$cpush rbp%2$cmov rbp, rsp%2$cmov rdi, format%2$cmov rsi, format%2$cmov rdx, 10%2$cmov rcx, 34%2$ccall printf%2$cmov rsp, rbp%2$cpop rbp%2$cxor rax, rax%2$cret%2$c%2$csection .data%2$cformat:%2$cdb %3$c%1$s%3$c, 0", 0
