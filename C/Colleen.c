5d64117aefe6c4929c289d6b8f87fe400d887ece
clorin <clorin@student.42.fr>
2023-07-06 19:16:51 +0200

    modif comment versoin /**/

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-//comment outside
+/*
+    This program will print its own source when run
+*/
 
 void write(char *str)
 {
@@ -8,8 +10,10 @@ void write(char *str)
 
 int main(void)
 {
-    //comment in main
-    char *x = "#include <stdio.h>%1$c//comment outside%1$c%1$cvoid write(char *str)%1$c{%1$c    printf(str, 10, 34, str);%1$c}%1$c%1$cint main(void)%1$c{%1$c    //comment in main%1$c    char *x = %2$c%3$s%2$c;%1$c    write(x);%1$c    return 0;%1$c}";
+    /*
+        comment in main
+    */
+    char *x = "#include <stdio.h>%1$c/*%1$c    This program will print its own source when run%1$c*/%1$c%1$cvoid write(char *str)%1$c{%1$c    printf(str, 10, 34, str);%1$c}%1$c%1$cint main(void)%1$c{%1$c    /*%1$c        comment in main%1$c    */%1$c    char *x = %2$c%3$s%2$c;%1$c    write(x);%1$c    return 0;%1$c}";
     write(x);
     return 0;
 }
