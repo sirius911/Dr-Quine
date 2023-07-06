d9d2f016171fc91ff216b8783c1bbc7dd74280b0
clorin <clorin@student.42.fr>
2023-07-06 18:32:24 +0200

    add return 0 in Colleen

--- C/Colleen.c
+++ C/Colleen.c
@@ -9,6 +9,7 @@ void write(char *str)
 int main(void)
 {
     //comment in main
-    char *x = "#include <stdio.h>%1$c//comment outside%1$c%1$cvoid write(char *str)%1$c{%1$c    printf(str, 10, 34, str);%1$c}%1$c%1$cint main(void)%1$c{%1$c    //comment in main%1$c    char *x = %2$c%3$s%2$c;%1$c    write(x);%1$c}";
+    char *x = "#include <stdio.h>%1$c//comment outside%1$c%1$cvoid write(char *str)%1$c{%1$c    printf(str, 10, 34, str);%1$c}%1$c%1$cint main(void)%1$c{%1$c    //comment in main%1$c    char *x = %2$c%3$s%2$c;%1$c    write(x);%1$c    return 0;%1$c}";
     write(x);
+    return 0;
 }
