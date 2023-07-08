d9385bb4b5764a37349e2e9db242ad92b1b9d4ce
clorin <clorin@student.42.fr>
2023-07-08 17:32:32 +0200

    fix ASM

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,22 +1,29 @@
-;colleen
+;colleen comment outside
 
 section .text
 global main
 extern printf
 
-main:
-push rbp
-mov rbp, rsp
-mov rdi, format
-mov rsi, format
-mov rdx, 10
-mov rcx, 34
-call printf
-mov rsp, rbp
-pop rbp
-xor rax, rax
-ret
+print:
+    push rbp
+    mov rbp, rsp
+    mov rdi, format
+    mov rsi, format
+    mov rdx, 10
+    mov rcx, 34
+    call printf
+    mov rsp, rbp
+    pop rbp
+    ret
+
+main:     ;comment in main
+    push rbp
+    mov rbp, rsp
+    call print
+    xor rax, rax
+    pop rbp
+    ret
 
 section .data
 format:
-db ";colleen%2$c%2$csection .text%2$cglobal main%2$cextern printf%2$c%2$cmain:%2$cpush rbp%2$cmov rbp, rsp%2$cmov rdi, format%2$cmov rsi, format%2$cmov rdx, 10%2$cmov rcx, 34%2$ccall printf%2$cmov rsp, rbp%2$cpop rbp%2$cxor rax, rax%2$cret%2$c%2$csection .data%2$cformat:%2$cdb %3$c%1$s%3$c, 0", 0
+db ";colleen comment outside%2$c%2$csection .text%2$cglobal main%2$cextern printf%2$c%2$cprint:%2$c    push rbp%2$c    mov rbp, rsp%2$c    mov rdi, format%2$c    mov rsi, format%2$c    mov rdx, 10%2$c    mov rcx, 34%2$c    call printf%2$c    mov rsp, rbp%2$c    pop rbp%2$c    ret%2$c%2$cmain:     ;comment in main%2$c    push rbp%2$c    mov rbp, rsp%2$c    call print%2$c    xor rax, rax%2$c    pop rbp%2$c    ret%2$c%2$csection .data%2$cformat:%2$cdb %3$c%1$s%3$c, 0", 0
--- ASM/Sully.s
+++ ASM/Sully.s
@@ -1,5 +1,5 @@
 section .data
-data: db "section .data%3$cdata: db %4$c%1$s%4$c, 0%3$cfilename_buf: db 0%3$cfilename: db %4$cSully_%5$cd.s%4$c, 0%3$cmode: db %4$cw+%4$c, 0%3$ccmd_buf: db 0%3$ccompil: db %4$cnasm -f elf64 Sully_%5$c1$d.s && gcc Sully_%5$c1$d.o -o Sully_%5$c1$d -no-pie%4$c, 0%3$cexec: db %4$c./Sully_%5$c1$d%4$c, 0%3$c%3$csection .text%3$cglobal main%3$cextern fopen%3$cextern fclose%3$cextern sprintf%3$cextern fprintf%3$cextern system%3$c%3$cmain:%3$cpush r12%3$cmov r12, %2$d%3$cdec r12%3$c%3$cmov rdi, filename_buf%3$cmov rsi, filename%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, filename_buf%3$cmov rsi, mode%3$ccall fopen%3$cpush rax%3$c%3$cmov rdi, rax%3$cmov rsi, data%3$cmov rdx, data%3$cmov rcx, r12%3$cmov r8, 10%3$cmov r9, 34%3$cpush 37%3$ccall fprintf%3$cpop rax%3$c%3$cpop rdi%3$ccall fclose%3$c%3$cmov rdi, cmd_buf%3$cmov rsi, compil%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, cmd_buf%3$ccall system%3$c%3$ccmp r12, 0%3$cjle end%3$c%3$cmov rdi, cmd_buf%3$cmov rsi, exec%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, cmd_buf%3$ccall system%3$c%3$cend:%3$cpop r12%3$cret", 0
+data: db "section .data%3$cdata: db %4$c%1$s%4$c, 0%3$cfilename_buf: db 0%3$cfilename: db %4$cSully_%5$cd.s%4$c, 0%3$cmode: db %4$cw+%4$c, 0%3$ccmd_buf: db 0%3$ccompil: db %4$cnasm -f elf64 Sully_%5$c1$d.s && gcc Sully_%5$c1$d.o -o Sully_%5$c1$d -no-pie%4$c, 0%3$cexec: db %4$c./Sully_%5$c1$d%4$c, 0%3$c%3$csection .text%3$cglobal main%3$cextern fopen%3$cextern fclose%3$cextern sprintf%3$cextern fprintf%3$cextern system%3$c%3$cmain:%3$cpush r12%3$cmov r12, %2$d%3$cmov r13, r12%3$cdec r13%3$c%3$cmov rdi, filename_buf%3$cmov rsi, filename%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, filename_buf%3$cmov rsi, mode%3$ccall fopen%3$cpush rax%3$c%3$cmov rdi, rax%3$cmov rsi, data%3$cmov rdx, data%3$cmov rcx, r13%3$cmov r8, 10%3$cmov r9, 34%3$cpush 37%3$ccall fprintf%3$cpop rax%3$c%3$cpop rdi%3$ccall fclose%3$c%3$cmov rdi, cmd_buf%3$cmov rsi, compil%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, cmd_buf%3$ccall system%3$c%3$ccmp r13, -1%3$cjle end%3$c%3$cmov rdi, cmd_buf%3$cmov rsi, exec%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, cmd_buf%3$ccall system%3$c%3$cend:%3$cpop r12%3$cret", 0
 filename_buf: db 0
 filename: db "Sully_%d.s", 0
 mode: db "w+", 0
@@ -18,7 +18,8 @@ extern system
 main:
 push r12
 mov r12, 5
-dec r12
+mov r13, r12
+dec r13
 
 mov rdi, filename_buf
 mov rsi, filename
@@ -33,7 +34,7 @@ push rax
 mov rdi, rax
 mov rsi, data
 mov rdx, data
-mov rcx, r12
+mov rcx, r13
 mov r8, 10
 mov r9, 34
 push 37
@@ -51,7 +52,7 @@ call sprintf
 mov rdi, cmd_buf
 call system
 
-cmp r12, 0
+cmp r13, -1
 jle end
 
 mov rdi, cmd_buf
