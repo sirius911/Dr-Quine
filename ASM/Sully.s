3954bfdd17e71e5a26d602163a4ac363c857584a
clorin <clorin@student.42.fr>
2023-07-10 17:11:54 +0200

    fix .o in asm and Sully.py

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -1,10 +1,10 @@
 section .data
-data: db "section .data%3$cdata: db %4$c%1$s%4$c, 0%3$cfilename_buf: db 0%3$cfilename: db %4$cSully_%5$cd.s%4$c, 0%3$cmode: db %4$cw+%4$c, 0%3$ccmd_buf: db 0%3$ccompil: db %4$cnasm -f elf64 Sully_%5$c1$d.s && gcc Sully_%5$c1$d.o -o Sully_%5$c1$d -no-pie%4$c, 0%3$cexec: db %4$c./Sully_%5$c1$d%4$c, 0%3$c%3$csection .text%3$cglobal main%3$cextern fopen%3$cextern fclose%3$cextern sprintf%3$cextern fprintf%3$cextern system%3$c%3$cmain:%3$cpush r12%3$cmov r12, %2$d%3$cmov r13, r12%3$cdec r13%3$c%3$cmov rdi, filename_buf%3$cmov rsi, filename%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, filename_buf%3$cmov rsi, mode%3$ccall fopen%3$cpush rax%3$c%3$cmov rdi, rax%3$cmov rsi, data%3$cmov rdx, data%3$cmov rcx, r13%3$cmov r8, 10%3$cmov r9, 34%3$cpush 37%3$ccall fprintf%3$cpop rax%3$c%3$cpop rdi%3$ccall fclose%3$c%3$cmov rdi, cmd_buf%3$cmov rsi, compil%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, cmd_buf%3$ccall system%3$c%3$ccmp r13, -1%3$cjle end%3$c%3$cmov rdi, cmd_buf%3$cmov rsi, exec%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, cmd_buf%3$ccall system%3$c%3$cend:%3$cpop r12%3$cret", 0
+data: db "section .data%3$cdata: db %4$c%1$s%4$c, 0%3$cfilename_buf: db 0%3$cfilename: db %4$cSully_%5$cd.s%4$c, 0%3$cmode: db %4$cw+%4$c, 0%3$ccmd_buf: db 0%3$ccompil: db %4$cnasm -f elf64 Sully_%5$c1$d.s && gcc Sully_%5$c1$d.o -o Sully_%5$c1$d -no-pie && rm -f Sully_%5$c1$d.o%4$c, 0%3$cexec: db %4$c./Sully_%5$c1$d%4$c, 0%3$c%3$csection .text%3$cglobal main%3$cextern fopen%3$cextern fclose%3$cextern sprintf%3$cextern fprintf%3$cextern system%3$c%3$cmain:%3$cpush r12%3$cmov r12, %2$d%3$cmov r13, r12%3$cdec r13%3$c%3$cmov rdi, filename_buf%3$cmov rsi, filename%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, filename_buf%3$cmov rsi, mode%3$ccall fopen%3$cpush rax%3$c%3$cmov rdi, rax%3$cmov rsi, data%3$cmov rdx, data%3$cmov rcx, r13%3$cmov r8, 10%3$cmov r9, 34%3$cpush 37%3$ccall fprintf%3$c%3$cpop rax%3$cpop rdi%3$ccall fclose%3$c%3$ccmp r12, 0    ;exit%3$cjle end%3$c%3$cmov rdi, cmd_buf%3$cmov rsi, compil%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, cmd_buf%3$ccall system%3$c%3$cmov rdi, cmd_buf%3$cmov rsi, exec%3$cmov rdx, r12%3$ccall sprintf%3$c%3$cmov rdi, cmd_buf%3$ccall system%3$c%3$cend:%3$cpop r12%3$cret", 0
 filename_buf: db 0
 filename: db "Sully_%d.s", 0
 mode: db "w+", 0
 cmd_buf: db 0
-compil: db "nasm -f elf64 Sully_%1$d.s && gcc Sully_%1$d.o -o Sully_%1$d -no-pie", 0
+compil: db "nasm -f elf64 Sully_%1$d.s && gcc Sully_%1$d.o -o Sully_%1$d -no-pie && rm -f Sully_%1$d.o", 0
 exec: db "./Sully_%1$d", 0
 
 section .text
@@ -39,10 +39,13 @@ mov r8, 10
 mov r9, 34
 push 37
 call fprintf
-pop rax
 
+pop rax
 pop rdi
 call fclose
+
+cmp r12, 0    ;exit
+jle end
 
 mov rdi, cmd_buf
 mov rsi, compil
@@ -51,9 +54,6 @@ call sprintf
 
 mov rdi, cmd_buf
 call system
-
-cmp r13, -1
-jle end
 
 mov rdi, cmd_buf
 mov rsi, exec
